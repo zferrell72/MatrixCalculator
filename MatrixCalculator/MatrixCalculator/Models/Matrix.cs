95929d1e08ebe636028d66108e2fea8cb2d1b449
ZFerrell72 <zferrell@student.neumont.edu>
2014-02-27 16:49:02 -0700

    Added helper methods for Matrix.cs
    Signed-off-by: ZFerrell72 <zferrell@student.neumont.edu>

--- MatrixCalculator/MatrixCalculator/Models/Matrix.cs
+++ MatrixCalculator/MatrixCalculator/Models/Matrix.cs
@@ -9,17 +9,38 @@ namespace MatrixCalculator.Models
     class Matrix
     {
         private int[,] theMatrix;
+        private int size;
 
         public Matrix(int dimension)
         {
             InitializeMatrix(dimension);
         }
 
+        public void PopulateMatrix(int[,] array)
+        {
+
+        }
+
         public void InitializeMatrix(int dimension)
         {
+            size = dimension;
             theMatrix = new int[dimension, dimension];
         }
 
+        public Matrix SubtractMatrix(Matrix otherMatrix)
+        {
+            return null;   
+        }
+
+        public int getValue(int row, int col)
+        {
+            return theMatrix[col, row];
+        }
+
+        public void setValue(int row, int col, int value)
+        {
+            theMatrix[col, row] = value;
+        }
 
     }
 }
