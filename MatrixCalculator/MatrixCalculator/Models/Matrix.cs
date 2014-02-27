960c1c406786ba61782bfb44ff79ae7bf4f12aab
ZFerrell72 <zferrell@student.neumont.edu>
2014-02-27 16:39:43 -0700

    Commit of the Matrix class
    Signed-off-by: ZFerrell72 <zferrell@student.neumont.edu>

---
+++ MatrixCalculator/MatrixCalculator/Models/Matrix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixCalculator.Models
+{
+    class Matrix
+    {
+        private int[,] theMatrix;
+
+        public Matrix(int dimension)
+        {
+            InitializeMatrix(dimension);
+        }
+
+        public void InitializeMatrix(int dimension)
+        {
+            theMatrix = new int[dimension, dimension];
+        }
+
+
+    }
+}
